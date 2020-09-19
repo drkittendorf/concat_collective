2cd4e6785f85ad7866e014bf9652237dfa7757b0
Erik De Luna <codingerik1.gmail.com>
2020-09-18 22:05:30 -0500

    adding the SearchBarComponent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,36 @@
-import React from "react"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-// search bar component for the page 
-function SearchBar(){
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: '25ch',
+  },
+}));
 
+export default function LayoutTextFields() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <TextField
+          id="standard-full-width"
+          label="Search here"
+          style={{ margin: 8 }}
+          placeholder="Javascript"
+          helperText="Press enter to submit!!"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+    </div>
+  );
 }
-
-export default SearchBar; 
