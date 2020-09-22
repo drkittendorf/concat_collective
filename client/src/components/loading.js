d48be25b0da8de64ce2254edde67856a8953b2ec
Erik De Luna <codingerik1.gmail.com>
2020-09-22 16:41:58 -0500

    adding loader

--- client/src/components/loading.js
+++ client/src/components/loading.js
@@ -1,11 +1,9 @@
 import React from "react";
 const loading =
-  "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
+    "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
 
-const Loading = () => (
-  <div className="spinner">
-    <img src={loading} alt="Loading" />
-  </div>
-);
+function Loading() {
+    return <img src={loading} alt="Loading" />
+};
 
 export default Loading;
