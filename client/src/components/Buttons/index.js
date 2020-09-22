af1c5323619ae6332162dee39440e7b38e5c735b
Erik De Luna <codingerik1.gmail.com>
2020-09-22 16:53:00 -0500

    setting up login and logout components

---
+++ client/src/components/Buttons/index.js
@@ -0,0 +1,6 @@
+import LoginButton from "./login-button";
+import LogoutButton from "./logout-button";
+
+
+// using a barrel to export all of the modules within components
+export { LoginButton, LogoutButton };
