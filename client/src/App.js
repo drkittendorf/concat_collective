2579773218355a546ed3c5fe6721481a2fe7fb27
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-09-22 17:15:40 -0500

    minor tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import Login from './pages/Login'
 import Home from './pages/Home'
 import NoMatch from './pages/NoMatch'
