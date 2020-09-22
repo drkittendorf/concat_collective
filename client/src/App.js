fb0ea5709c806082b536f07720f3e6f4c13773ad
Erik De Luna <codingerik1.gmail.com>
2020-09-22 00:08:42 -0500

    implementing autho

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Login from './pages/Login'
 import Home from './pages/Home'
 import NoMatch from './pages/NoMatch'
 
 function App() {
   return (
-     <Router>
-     <div>
-       <Switch>
-         <Route exact path={["/", "/login"]} component={Login} />
-         <Route exact path={["/home"]} component={Home} />
-         <Route exact path="/*" component={NoMatch} />
-       </Switch>
-     </div>
-   </Router>
+    <div>
+      <Switch>
+        <Route exact path={["/", "/login"]} component={Login} />
+        <Route exact path={["/home"]} component={Home} />
+        <Route exact path="/*" component={NoMatch} />
+      </Switch>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Auth0Provider>
+      <App />
+    </Auth0Provider>
+  </Router>,
   document.getElementById('root')
 );
 
