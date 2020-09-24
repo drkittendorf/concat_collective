b936977dd5cc263fd7487edacb9352f45d3f73f8
Lia <liavibanez@gmail.com>
2020-09-23 22:30:52 -0500

    adding + button

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	button: {
+    margin: theme.spacing(1.5),
+    color: 'white',
+	},
+}));
 
 function LoginButton() {
+  //to use styles
+  const classes = useStyles();
+
   const { loginWithRedirect } = useAuth0();
+
   return (
     <Button
       onClick={() => loginWithRedirect()}
       variant="primary"
-      className="btn-margin"
+      className={classes.button}
     >
       Log In
     </Button>
