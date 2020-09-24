93a7fe4947dc68355e730e366a53c523a03ab63d
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-09-23 21:43:22 -0500

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+<<<<<<< HEAD
 import Login from './pages/Login'
+=======
+import { useAuth0 } from "@auth0/auth0-react";
+
+import PrivateRoute from './components/PrivateRoute'
+>>>>>>> 96d806f9816fadce255f7123899de6f9f6cb34c9
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import NoMatch from './pages/NoMatch'
@@ -17,8 +23,8 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path={["/", "/login"]} component={Login} />
-        <Route exact path="/home" component={Home} />
+        <Route exact path={["/", "/home"]} component={Home} />
+        {/* <Route exact path="/home" component={Home} /> */}
         <PrivateRoute path="/profile" component={Profile} />
         <Route exact path="*" component={NoMatch} />
       </Switch>
