66e94e24a0637b9c909629e3156b276f5acb5e7c
Erik De Luna <codingerik1.gmail.com>
2020-09-23 21:55:28 -0500

    adding dummy data to test the cards

--- client/src/components/loading.js
+++ client/src/components/loading.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const loading =
+const loadingLink =
   "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
 
 function Loading() {
   return <div className="spinner">
-    <img src={loading} alt="Loading" />
+    <img src={loadingLink} alt="Loading" />
   </div>
 };
 
