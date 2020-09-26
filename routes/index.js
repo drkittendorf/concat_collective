f4a6b7ca7d7bfa105029611157b0aa4a2132c2e8
Mark McDonald <markwilliammcdonald@gmail.com>
2020-09-26 14:14:09 -0500

    changed typos

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const router = require('express').Router();
 const resourceRoutes = require('./api/resource');
-const snippetRoutes = require('api/snippets')
+const snippetRoutes = require('./api/snippet')
 
 router.use('/api/resources', resourceRoutes);
 router.use('/api/snippets', snippetRoutes)
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
