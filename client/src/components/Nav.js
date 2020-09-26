e0a53c3bb893243831c46fdfe072b71d75f84d26
Lia <liavibanez@gmail.com>
2020-09-26 11:44:37 -0500

    putting links on other side of navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,9 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 // import Link from "@material-ui/core/Link";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import AddResourceModal from './AddResourceModal';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './LoginButton';
@@ -17,19 +17,23 @@ import InputIcon from '@material-ui/icons/Input';
 
 const useStyles = makeStyles({
 	link: {
-		color: "white",
-		padding: "0px 15px 0px 15px",
+		color: 'white',
+		padding: '0px 15px 0px 15px',
 	},
 	button: {
 		// margin: theme.spacing(1.5),
 		color: 'white',
 	},
-	Logo:{
+	Logo: {
 		color: 'white',
-		fontSize: '20px'
+		fontSize: '20px',
 	},
-	black:{
-		backgroundColor: 'black'
+	black: {
+		backgroundColor: 'black',
+	},
+	space: {
+		display: 'flex',
+		justifyContent: 'space-between'
 	}
 });
 
@@ -38,29 +42,28 @@ function Nav() {
 	const { isAuthenticated } = useAuth0();
 
 	return (
-		<AppBar position="static" className={classes.black} >
-			<Toolbar className={classes.blue}>
+		<AppBar position='static' className={classes.black}>
+			<Toolbar className={classes.space}>
 				{/* <Typography variant="h6" className={classes.title}>
 					concat
 				</Typography> */}
-				<Button className={classes.Logo} component={Link} to='/'>
-					concat()
-				</Button>
-				{/* <Link href="/" className={classes.link}>
-					Search
-				</Link>
-				<Link href="/saved" className={classes.link}>
-					Saved
-				</Link> */}
-				<AddResourceModal />
-				<Button className={classes.button} component={Link} to='/profile'>
-					<PersonIcon fontSize='large' />
-				</Button>
-				<Button className={classes.button} component={Link} to='/home'>
-					<HomeIcon fontSize='large' />
-				</Button>
-				{/* maybe we can put an icon here  */}
-				{isAuthenticated ? <LogoutButton /> : <LoginButton />}
+				<div>
+					<Button className={classes.Logo} component={Link} to='/'>
+						concat(collective)
+					</Button>
+				</div>
+
+				<div>
+					<AddResourceModal />
+					<Button className={classes.button} component={Link} to='/profile'>
+						<PersonIcon fontSize='large' />
+					</Button>
+					<Button className={classes.button} component={Link} to='/home'>
+						<HomeIcon fontSize='large' />
+					</Button>
+					{/* maybe we can put an icon here  */}
+					{isAuthenticated ? <LogoutButton /> : <LoginButton />}
+				</div>
 			</Toolbar>
 		</AppBar>
 	);
