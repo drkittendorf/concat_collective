8c56b5ad5602a12ad5c0cb2e02e4daeb60487712
Erik De Luna <codingerik1.gmail.com>
2020-09-28 16:58:12 -0500

    two options for implementing the accordion

--- client/src/components/CodeJar/CodeJar.js
+++ client/src/components/CodeJar/CodeJar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 // You can choose to use the component or the hook
 import { ReactCodeJar, useCodeJar } from "react-codejar";
 import "./index.css";
@@ -20,7 +21,7 @@ import AddCircleIcon from '@material-ui/icons/AddCircle';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -31,8 +32,20 @@ const useStyles = makeStyles((theme) => ({
     button: {
         // margin: theme.spacing(1.5),
         color: 'white',
+    },
+    details: {
+        // mixHeight: '10px',
     }
 }));
+
+// const AccordionDetails = withStyles((theme) => ({
+//     root: {
+//       padding: theme.spacing(2),
+//       height: "20px"
+//     },
+//   }))(MuiAccordionDetails);
+
+
 
 
 const highlight = editor => {
@@ -93,7 +106,7 @@ const CodeJar = () => {
                         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
                             <Typography>view preview here 🧐 </Typography>
                         </AccordionSummary>
-                        <AccordionDetails>
+                        <AccordionDetails className={classes.details} >
                             <div className='editor'>
                                 <ReactCodeJar
                                     code={code} // Initial code value
@@ -145,3 +158,102 @@ const CodeJar = () => {
 
 export default CodeJar;
 
+
+// todo: implement the custom if the team doesn't like the regular 
+// *** accordion? 
+// import React from 'react';
+// import { withStyles } from '@material-ui/core/styles';
+// import MuiAccordion from '@material-ui/core/Accordion';
+// import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+// import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+// import Typography from '@material-ui/core/Typography';
+
+// const Accordion = withStyles({
+//   root: {
+//     border: '1px solid rgba(0, 0, 0, .125)',
+//     boxShadow: 'none',
+//     '&:not(:last-child)': {
+//       borderBottom: 0,
+//     },
+//     '&:before': {
+//       display: 'none',
+//     },
+//     '&$expanded': {
+//       margin: 'auto',
+//     },
+//   },
+//   expanded: {},
+// })(MuiAccordion);
+
+// const AccordionSummary = withStyles({
+//   root: {
+//     backgroundColor: 'rgba(0, 0, 0, .03)',
+//     borderBottom: '1px solid rgba(0, 0, 0, .125)',
+//     marginBottom: -1,
+//     minHeight: 56,
+//     '&$expanded': {
+//       minHeight: 56,
+//     },
+//   },
+//   content: {
+//     '&$expanded': {
+//       margin: '12px 0',
+//     },
+//   },
+//   expanded: {},
+// })(MuiAccordionSummary);
+
+// const AccordionDetails = withStyles((theme) => ({
+//   root: {
+//     padding: theme.spacing(2),
+//   },
+// }))(MuiAccordionDetails);
+
+// export default function CustomizedAccordions() {
+//   const [expanded, setExpanded] = React.useState('panel1');
+
+//   const handleChange = (panel) => (event, newExpanded) => {
+//     setExpanded(newExpanded ? panel : false);
+//   };
+
+//   return (
+//     <div>
+//       <Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+//         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+//           <Typography>Collapsible Group Item #1</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+//             elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//       <Accordion square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+//         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+//           <Typography>Collapsible Group Item #2</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+//             elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//       <Accordion square expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+//         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+//           <Typography>Collapsible Group Item #3</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+//             elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//     </div>
+//   );
+// }
