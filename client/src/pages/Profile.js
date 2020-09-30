3991de7c283f3e05fd7fd6d1696375b1c8c2211d
Lia <liavibanez@gmail.com>
2020-09-30 18:47:26 -0500

    updating profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,51 +2,66 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { useAuth0 } from "@auth0/auth0-react";
-import SearchBar from "../components/SearchBar"
-
-
+import { useAuth0 } from '@auth0/auth0-react';
+import SearchBar from '../components/SearchBar';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        // textAlign: 'center',
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		// textAlign: 'center',
         color: theme.palette.text.secondary,
+        display: 'flex',
+        width: '50%',
+        alignItems: 'center'
+	},
+	img: {
+		height: '10rem',
+        borderRadius: '50%',
+        padding: '0px 20px 0px 0px '
     },
-    img:{
-        height: '7rem',
-        borderRadius: '50%'
-    }
+    imgContainer:{
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+    headerContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 }));
 
 function Profile() {
-    const classes = useStyles();
-    const { user } = useAuth0();
-    const { name, picture, email } = user;
+	const classes = useStyles();
+	const { user } = useAuth0();
+	const { name, picture, email } = user;
 
-    console.log(user); 
+	console.log(user);
 
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <img
-                            className={classes.paper}
-                            src={picture}
-                            alt="Profile"
-                            className={classes.img}
-                        />
-                         <h2>{name}</h2>
-                        <p >{email}</p>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </div>
-    );
+	return (
+		<div className={classes.root}>
+			<Grid container spacing={3}>
+				<Grid item xs={12} className={classes.headerContainer}>
+					<Paper className={classes.paper}>
+						<Grid item xs={6} className={classes.imgContainer}>
+							<img
+								className={classes.paper}
+								src={picture}
+								alt='Profile'
+								className={classes.img}
+							/>
+						</Grid>
+						<Grid item xs={6} >
+							<h2>{name}</h2>
+							<p>{email}</p>
+						</Grid>
+					</Paper>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default Profile;
