8cd7317db4935aeaf4faf40ee5ebcf757fb876be
Mark McDonald <markwilliammcdonald@gmail.com>
2020-09-30 21:10:01 -0500

    fixed the server js problems and env fixes

--- routes/api/userAPI.js
+++ routes/api/userAPI.js
@@ -4,8 +4,8 @@ const userController = require('../../controllers/userController.js');
 
 router.route('/:id')
     .get(userController.findById)
-    .post(userController.create)
-    .put(userController.update)
-    .delete(userController.remove);
+    .post(userController.create);
+// .put(userController.update)
+// .delete(userController.remove);
 
 module.exports = router;
