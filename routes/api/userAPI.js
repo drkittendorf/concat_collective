3a5ca06f8afe1341aec6047888b279cf184a7752
Mark McDonald <markwilliammcdonald@gmail.com>
2020-09-30 19:48:16 -0500

    added a POST route for users

--- routes/api/userAPI.js
+++ routes/api/userAPI.js
@@ -4,6 +4,7 @@ const userController = require('../../controllers/userController.js');
 
 router.route('/:id')
     .get(userController.findById)
+    .post(userController.create)
     .put(userController.update)
     .delete(userController.remove);
 
