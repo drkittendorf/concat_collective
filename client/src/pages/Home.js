8c3ec4222d5bc680703be957321d150be755d908
Erik De Luna <codingerik1.gmail.com>
2020-10-02 16:56:16 -0500

    fixed bug in adding functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,9 +7,8 @@ import Carousel from '../components/Carousel/Carousel'
 import BookmarkCards from '../components/BookmarkCards/BookmarkCards'
 import CodeJar from '../components/CodeJar/CodeJar'
 
-import data from '../dummyData.json'
 import Api from '../utils/API'
-import Transform from '../utils/Transform'
+import transform from '../utils/transform'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -32,35 +31,43 @@ const useStyles = makeStyles((theme) => ({
 export default function FullWidthGrid() {
   const classes = useStyles();
   const [bookmarkCards, setBookmarkCards] = useState([]);
-  const [codeCards, setCodeCards] = useState([]);
-
-  /// ******  transformers [done]
+  const [codeCards, setCodeCards] = useState({});
 
   useEffect(() => {
     Api.getBookmarks().then(res => setBookmarkCards(res.data))
-    Api.getSnippets().then(res => setCodeCards(res.data))
+    Api.getSnippets().then(res => {
+
+      const cardData = transform.toObject(res.data)
+      setCodeCards(cardData)
+    }) 
   }, [])
 
-  // homepage closure
-  // id of the card 
-  // change setcodecards send and object with the change 
   const handleAdd = (id) => (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     // find the card in the array 
     // ** then send it to the user database 
     // dispatch(deleteUser(id));
 
     // turn array to object? 
-    let card = Transform.toObject(bookmarkCards.concat(codeCards))
+    let card = transform.toObject(bookmarkCards.concat(transform.toArray(codeCards)))
     // send this object to the be saved as a new entry for the user database 
     // so this will probably need to be pushed to the array in the in the database 
-    if(card[id].snippet){
+    if (card[id].snippet) {
       console.log('this is a code card send it here')
-      console.log(card[id]); 
-    }else{
+      console.log(card[id]);
+    } else {
       console.log('this is not a code card send it here')
-      console.log(card[id]); 
+      console.log(card[id]);
     }
+  }
+
+  const setCodeWrapper = (id) => (snippet) => {
+    // where is the snippet parameter being used in this fucntion 
+    // and do we need it? 
+
+    // just setting each card to have an id as the key and then
+    // then rest of the object as the value of the key 
+    setCodeCards({...codeCards, [id]: { ...codeCards[id], snippet } })
   }
 
   return (
@@ -73,16 +80,18 @@ export default function FullWidthGrid() {
         </Grid>
         <Grid item xs={12} container spacing={3} justify="flex-start" >
           {bookmarkCards.map(card => {
-            return <BookmarkCards 
-            key={card._id} {...card} 
-            handleAdd={handleAdd}  
+            return <BookmarkCards
+              key={card._id} {...card}
+              handleAdd={handleAdd}
             />
           })}
-          {codeCards.map(card => {
-            return <CodeJar 
-            key={card._id} {...card}
-            handleAdd={handleAdd}  
-             />
+          {Object.keys(codeCards).map(key => {
+            const card = codeCards[key]
+            return <CodeJar
+              key={card._id} {...card}
+              handleAdd={handleAdd}
+              setCode={setCodeWrapper(card._id)}
+            />
           })}
         </Grid>
       </Grid>
