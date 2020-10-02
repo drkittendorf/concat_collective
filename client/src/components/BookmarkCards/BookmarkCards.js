f305fbd13591b064db622136fbf5ba599b743012
Lia <liavibanez@gmail.com>
2020-10-02 18:34:51 -0500

    adding link preview

--- client/src/components/BookmarkCards/BookmarkCards.js
+++ client/src/components/BookmarkCards/BookmarkCards.js
@@ -6,76 +6,82 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import "./style.css";
+import './style.css';
 
 import IconButton from '@material-ui/core/IconButton';
 import CardHeader from '@material-ui/core/CardHeader';
 import Icon from '@material-ui/core/Icon';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { ReactTinyLink } from 'react-tiny-link';
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    borderRadius: '10px',
-    height: '220px',
-    backgroundColor: '#ECF0F1'
+	root: {
+		minWidth: 275,
+		borderRadius: '10px',
+		backgroundColor: '#ECF0F1',
+	},
+	bullet: {
+		display: 'inline-block',
+		margin: '0 2px',
+		transform: 'scale(0.8)',
+	},
+	skill: {
+		fontSize: 14,
+		maring: '20px',
+	},
+	pos: {
+		marginBottom: 12,
+	},
+	icon: {
+    marginLeft: '100px',
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  skill: {
-    fontSize: 14,
-    maring: '20px'
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  icon: {
-    marginLeft: '100px'
-  }
 });
 
 export default function SimpleCard(props) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const { title, link, _id, category, skill, handleAdd } = props;
+	const { title, link, _id, category, skill, handleAdd } = props;
 
-  return (
-    <Grid item xs={12} sm={6} >
-      <Card className={classes.root}>
-        <CardHeader
-          action={
-            <IconButton aria-label="settings"
-              onClick={handleAdd(_id)}
-            >
-              {/* <MoreVertIcon /> */}
-              <Icon className="fas fa-bookmark" />
-            </IconButton>
-          }
-          title={title || 'this will be the title'}
-          subheader={skill || 'this would be the skill level '}
-        />
-        <CardContent>
-          <Typography className={classes.pos} color="textSecondary">
-            {category || 'this will be the category'}
-          </Typography>
-          <Typography variant="body2" component="p">
-            <br />
-            <Button target="_blank" href={link || 'https://www.google.com'}>
-              <Icon className="fas fa-link" />
-            </Button>
-            <Button size="small" onClick={handleAdd(_id)} >
-              {/* <Icon className="fas fa-plus" /> */}
-              <AddCircleIcon />
-              {/* add to collection */}
-            </Button>
-          </Typography>
-        </CardContent>
-        <CardActions>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
+	return (
+		<Grid item xs={12} sm={6}>
+			<Card className={classes.root}>
+				<CardHeader
+					action={
+						<IconButton aria-label='settings' onClick={handleAdd(_id)}>
+							{/* <MoreVertIcon /> */}
+							<Icon className='fas fa-bookmark' />
+						</IconButton>
+					}
+					title={title || 'this will be the title'}
+					subheader={skill || 'this would be the skill level '}
+				/>
+				<CardContent>
+					<Typography variant='body2' component='p'>
+						<Grid item>
+							<ReactTinyLink
+								cardSize='large'
+								showGraphic={true}
+								maxLine={2}
+								minLine={1}
+								url={link}
+							/>
+						</Grid>
+
+						<Typography className={classes.pos} color='textSecondary'>
+							{category || 'this will be the category'}
+						</Typography>
+						<Button target='_blank' href={link || 'https://www.google.com'}>
+							<Icon className='fas fa-link' />
+						</Button>
+						<Button size='small' onClick={handleAdd(_id)}>
+							{/* <Icon className="fas fa-plus" /> */}
+							<AddCircleIcon />
+							{/* add to collection */}
+						</Button>
+					</Typography>
+				</CardContent>
+				<CardActions></CardActions>
+			</Card>
+		</Grid>
+	);
 }
