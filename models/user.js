6f1f972506b24febe1fb1f442172157510447356
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-10-03 11:23:10 -0500

    pull

--- models/user.js
+++ models/user.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId; //! Not 100% sure we need this...but
+// const ObjectId = Schema.ObjectId; //! Not 100% sure whether we need this...or not??
 
 
 // Create the structure of concat's User DB
 const userSchema = new Schema({
-    // stores the name of the user as a string
-    name: { type: String, required: true },
-    // stores email information
-    email: { type: String, required: true },
+ // store jwt object data here
+    jwt: {  
+        email: { type: String, required: true },
+        email_verified: '',
+        name: { type: String, required: true },
+        nickname: '',
+        picture: '',
+        sub: '',
+        updated_at: ''
+     },
     //! need arrays to store card and snippet arrays
     userBookmarks: [{
         type: Schema.Types.ObjectId, ref: 'Resource'
@@ -23,3 +29,8 @@ const userSchema = new Schema({
 const User = mongoose.model("User", userSchema);
 // Exports the Resource schema
 module.exports = User;
+
+// stores the name of the user as a string
+    // name: { type: String, required: true },
+    // stores email information
+    // email: { type: String, required: true },
