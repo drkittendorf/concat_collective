2d3b99c77975c451f2ee0feab78342a44b80aee7
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-10-02 21:12:02 -0500

    updated user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 
 // Create the structure of concat's User DB
@@ -9,8 +10,8 @@ const userSchema = new Schema({
     // stores email information
     email: { type: String, required: true },
     //! need arrays to store card and snippet arrays
-    // userBookmarks: [ Resource.Types.ObjectId ],
-    // userSnippets:  [ Snippet.Types.ObjectId ]
+    // userBookmarks: [ Schema.Types.ObjectId, ref: 'resource' ],
+    // userSnippets:  [ Schema.Types.ObjectId, ref: 'snippet' ]
 });
 // Defines the resource schema from above
 const User = mongoose.model("User", userSchema);
