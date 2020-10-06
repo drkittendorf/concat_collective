8178da89915a0b06c555dd13c6a2fd99e6e9eea2
Mark McDonald <markwilliammcdonald@gmail.com>
2020-10-05 22:56:37 -0500

    closure of the checkJwt middleware. everything Jeff thought would be a good change is commented out in the index.js file in the  api folder

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,27 @@ const router = require('express').Router();
 const resourceRoutes = require('./api/resource');
 const snippetRoutes = require('./api/snippet');
 const userRoutes = require('./api/userAPI');
+// const jwt = require('express-jwt');
+// const jwksRsa = require('jwks-rsa');
+
+// const checkJwt = () => jwt({
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `${issuer}.well-known/jwks.json`,
+
+//   }),
+
+//   audience: 'https://concat.api/',
+//   issuer: 'https://dev-2b7i14d1.us.auth0.com/',
+//   algorithms: ['RS256'],
+// });
 
 router.use('/api/resources', resourceRoutes);
 router.use('/api/snippets', snippetRoutes);
 router.use('/api/userAPI', userRoutes);
+// router.use('/api/userAPI', checkJwt, userRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
