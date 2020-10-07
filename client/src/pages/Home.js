e4485aa1a90c297b40ffb653eff8cd3ca9617ce0
Erik De Luna <codingerik1.gmail.com>
2020-10-07 16:01:38 -0500

    removing unneccessary lines

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -29,9 +29,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// data[0].
-
-export default function FullWidthGrid() {
+export default function Home() {
   const classes = useStyles();
   const [bookmarkCards, setBookmarkCards] = useState([]);
   const [codeCards, setCodeCards] = useState({});
